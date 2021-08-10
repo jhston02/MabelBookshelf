139ec5c88d9668757f063f1dabb3fe949cf19631
John H Stone <coleslaw.john@gmail.com>
2021-08-09 20:44:51 -0400

    Bookshelf tests added

---
+++ tests/MabelBookShelf.Bookshelf.Domain.Tests/BookshelfTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MockBookStore.Bookshelf.Domain.Aggregates.BookshelfAggregate;
+using Xunit;
+
+namespace MabelBookshelf.Bookshelf.Domain.Tests
+{
+    using Bookshelf=MockBookStore.Bookshelf.Domain.Aggregates.BookshelfAggregate.Bookshelf;
+    
+    public class BookshelfTests
+    {
+        private Bookshelf GetBookshelf(string name)
+        {
+            return new Bookshelf(name, 0);
+        }
+
+        [Fact]
+        public void Bookshelf_ChangeName_BookshelfHasNewName()
+        {
+            var bookshelf = GetBookshelf("test");
+            bookshelf.Rename("new");
+            Assert.Equal("new", bookshelf.Name);
+        }
+        
+        [Fact]
+        public void Bookshelf_AddBook_BookshelfHasBook()
+        {
+            var bookGuid = Guid.NewGuid();
+            var bookshelf = GetBookshelf("test");
+            bookshelf.AddBook(bookGuid);
+            Assert.Equal(bookGuid, bookshelf.Books.First());
+        }
+        
+        [Fact]
+        public void BookshelfHasBook_RemoveBook_BookshelfDoesNotHaveBook()
+        {
+            var bookGuid = Guid.NewGuid();
+            var bookshelf = GetBookshelf("test");
+            bookshelf.AddBook(bookGuid);
+            bookshelf.RemoveBook(bookGuid);
+            Assert.Empty(bookshelf.Books);
+        }
+
+        [Fact]
+        public void BookshelfDoesNotHaveBook_RemoveBook_ThrowsException()
+        {
+            var bookshelf = GetBookshelf("test");
+            Assert.Throws<BookshelfDomainException>(() => bookshelf.RemoveBook(Guid.NewGuid()));
+        }
+        
+        [Fact]
+        public void BookshelfHasBook_AddKnownBook_ThrowsException()
+        {
+            var bookGuid = Guid.NewGuid();
+            var bookshelf = GetBookshelf("test");
+            bookshelf.AddBook(bookGuid);
+            Assert.Throws<BookshelfDomainException>(() => bookshelf.AddBook(bookGuid));
+        }
+    }
+}
