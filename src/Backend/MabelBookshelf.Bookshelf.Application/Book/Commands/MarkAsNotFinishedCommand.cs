9c838625efc00c034328bf7388f23ca6836540ad
Luke Haggard <Luke_haggard@yahoo.com>
2021-09-07 21:11:42 -0400

    fix white space

--- src/Backend/MabelBookshelf.Bookshelf.Application/Book/Commands/MarkAsNotFinishedCommand.cs
+++ src/Backend/MabelBookshelf.Bookshelf.Application/Book/Commands/MarkAsNotFinishedCommand.cs
@@ -5,7 +5,6 @@ namespace MabelBookshelf.Bookshelf.Application.Book.Commands
     public class MarkAsNotFinishedCommand : IRequest<bool>
     {
         public string BookId { get; private set; }
-
         public MarkAsNotFinishedCommand(string bookId)
             { 
                 this.BookId = bookId;
